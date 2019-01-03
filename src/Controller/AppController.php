f7225bcff52ddb38116a59ce5836bbdbd44d06a0
komal-ficode <46098688+komal-ficode@users.noreply.github.com>
2019-01-03 18:06:30 +0530

    Update AppController.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -45,7 +45,7 @@ class AppController extends Controller
             'enableBeforeRedirect' => false,
         ]);
         $this->loadComponent('Flash');
-
+if(1){}
         /*
          * Enable the following component for recommended CakePHP security settings.
          * see https://book.cakephp.org/3.0/en/controllers/components/security.html
